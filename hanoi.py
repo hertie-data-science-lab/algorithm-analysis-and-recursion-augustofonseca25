ab529b6133fb733655368f2bdb58e90e87b0a9c0
Rodrigo Dornelles <31113760+rfdornelles@users.noreply.github.com>
2023-02-26 18:34:24 +0100

    cleaning the output and removing unused variables

--- hanoi.py
+++ hanoi.py
@@ -3,13 +3,9 @@
 @author: Augusto Fonseca and Rodrigo Dornelles
 """
 
-tower_A_orig = []
-tower_B_dest = []
-tower_C_aux = []
-
 def Tower_of_Hanoi(n_rings, origin, destination, aux): # Move the rings between towers
     # If you only have one ring, just move to the destination tower
-    print("O número n é:", n_rings)
+
     if n_rings == 1:
         print("Move ring 1 from tower", origin, "to tower", destination, ".")
         return
@@ -22,10 +18,10 @@ def Tower_of_Hanoi(n_rings, origin, destination, aux): # Move the rings between 
     a-) move the 1st one to the auxiliary Tower
     b-) move the 2nd one to the destination
     c-) recover the 1st ring from aux tower to the destination Tower, above the 2nd one.
-    Then the function does the reverse path (increasing the n by one unit) to repeat that movement, and so on. 
+    Then the function does the reverse path to repeat that movement, and so on. 
     '''
     # Attention to the parameters: the next line will move from origin to aux
-    Tower_of_Hanoi(n_rings-1, origin, aux, destination)
+    Tower_of_Hanoi(n_rings-1, origin = origin, aux, destination)
     #
     # Move the next ring from the origin to the destination
     print("Move ring", n_rings,  "from tower", origin, "to tower", destination, ".")
